d2dea74fe79354a76ed6deae4d0fec91ec2e72b4
Davy Lopes <d4vyl0pes@gmail.com>
2025-09-08 16:36:04 -0300

    feat: micro-services

---
+++ reservation-system.notification-service/src/main/java/com/davy/reservation_system/notification_service/Application.java
@@ -0,0 +1,13 @@
+package com.davy.reservation_system.notification_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ reservation-system.room-service/src/test/java/com/davy/reservation_system/room_service/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davy.reservation_system.room_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
