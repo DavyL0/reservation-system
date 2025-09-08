914a5877e560ec16febe9e07ad245e5073ea051a
Davy Lopes <d4vyl0pes@gmail.com>
2025-09-08 16:41:01 -0300

    feat: create User entitie and User Repositoy

---
+++ reservation-system.user-service/src/main/java/com/davy/reservation_system/user_service/entities/User.java
@@ -0,0 +1,19 @@
+package com.davy.reservation_system.user_service.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+    private String username;
+    private String email;
+    private String password;
+    private String role;
+
+
+}
---
+++ reservation-system.user-service/src/main/java/com/davy/reservation_system/user_service/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.davy.reservation_system.user_service.repository;
+
+import com.davy.reservation_system.user_service.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
