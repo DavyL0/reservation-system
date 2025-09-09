98087fced6ae32686df7dc00d7e1804f32e4fd49
Davy Lopes <d4vyl0pes@gmail.com>
2025-09-09 17:32:14 -0300

    update: add endpoints to acess and repository

--- reservation-system.reservation-service/src/main/java/com/davy/reservation_system/reservation_service/controller/ReservaControlller.java
+++ reservation-system.reservation-service/src/main/java/com/davy/reservation_system/reservation_service/controller/ReservaControlller.java
@@ -1,4 +1,44 @@
 package com.davy.reservation_system.reservation_service.controller;
 
+import com.davy.reservation_system.reservation_service.entities.Reserva;
+import com.davy.reservation_system.reservation_service.services.ReservaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/reservas")
 public class ReservaControlller {
+    @Autowired
+    private ReservaService reservaService;
+
+    @PostMapping
+    public ResponseEntity<?>  criarReserva(@RequestBody Reserva reserva)throws Exception {
+        try {
+            return  ResponseEntity.ok(reservaService.criarReserva(reserva));
+        }catch (Exception e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @GetMapping("/usuario/{userID}")
+    public List<Reserva> findByUserId(@PathVariable Long userID){
+        return reservaService.listarReservasByUserId(userID);
+    }
+
+    @GetMapping("/all")
+    public List<Reserva> findAll(){
+        return reservaService.listarReservas();
+    }
+
+    @PutMapping("/cancelar/{id}")
+    public ResponseEntity<?>  cancelarReserva(@PathVariable("id") Long id){
+        try {
+            return ResponseEntity.ok(reservaService.cancelarReserva(id));
+        }catch (Exception e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
 }
--- reservation-system.reservation-service/src/main/java/com/davy/reservation_system/reservation_service/repository/ReservaRepository.java
+++ reservation-system.reservation-service/src/main/java/com/davy/reservation_system/reservation_service/repository/ReservaRepository.java
@@ -1,4 +1,14 @@
 package com.davy.reservation_system.reservation_service.repository;
 
-public interface ReservaRepository {
+import com.davy.reservation_system.reservation_service.entities.Reserva;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+@Repository
+public interface ReservaRepository extends JpaRepository<Reserva, Long> {
+
+    List<Reserva> findTipoAndDataInicioBetween(String tipo, LocalDateTime inicio, LocalDateTime fim);
+    List<Reserva> findByUserId(Long userID);
 }
