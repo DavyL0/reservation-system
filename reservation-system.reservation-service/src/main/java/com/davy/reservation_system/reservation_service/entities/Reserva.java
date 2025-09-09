464dd4ba9ce219db7d8990ca31947611191676d5
Davy Lopes <d4vyl0pes@gmail.com>
2025-09-09 17:31:03 -0300

    update: add const and getters and setters

--- reservation-system.reservation-service/src/main/java/com/davy/reservation_system/reservation_service/entities/Reserva.java
+++ reservation-system.reservation-service/src/main/java/com/davy/reservation_system/reservation_service/entities/Reserva.java
@@ -1,4 +1,80 @@
 package com.davy.reservation_system.reservation_service.entities;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
 public class Reserva {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String tiulo; // Nome do evento ou da reserva
+    private String tipo; // Tipo: sala, quarto, evento etc.
+
+    private LocalDateTime dataInicio;
+    private LocalDateTime dataFim;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    private String status;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTiulo() {
+        return tiulo;
+    }
+
+    public void setTiulo(String tiulo) {
+        this.tiulo = tiulo;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public LocalDateTime getDataInicio() {
+        return dataInicio;
+    }
+
+    public void setDataInicio(LocalDateTime dataInicio) {
+        this.dataInicio = dataInicio;
+    }
+
+    public LocalDateTime getDataFim() {
+        return dataFim;
+    }
+
+    public void setDataFim(LocalDateTime dataFim) {
+        this.dataFim = dataFim;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
