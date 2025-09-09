a9cd83b3c03e062f0ee7b444f534280aa3a00ed7
Davy Lopes <d4vyl0pes@gmail.com>
2025-09-09 15:43:14 -0300

    fix: add the correct queue

--- reservation-system.notification-service/src/main/java/com/davy/reservation_system/notification_service/config/RabbitMQConfig.java
+++ reservation-system.notification-service/src/main/java/com/davy/reservation_system/notification_service/config/RabbitMQConfig.java
@@ -1,4 +1,14 @@
 package com.davy.reservation_system.notification_service.config;
 
+import org.springframework.amqp.core.Queue;
+import org.springframework.context.annotation.Bean;
+
 public class RabbitMQConfig {
+
+    public static final String USER_QUEUE = "user_queue";
+
+    @Bean
+    public Queue queue() {
+        return new Queue(USER_QUEUE, true);
+    }
 }
