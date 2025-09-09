1c9b7214c6ef68b2b5bf10d6d979de347f2837fd
Davy Lopes <d4vyl0pes@gmail.com>
2025-09-09 15:06:07 -0300

    update:  add more log informations and connect to RabbitMQConfig

--- reservation-system.user-service/src/main/java/com/davy/reservation_system/user_service/producer/UserProducer.java
+++ reservation-system.user-service/src/main/java/com/davy/reservation_system/user_service/producer/UserProducer.java
@@ -2,7 +2,10 @@ package com.davy.reservation_system.user_service.producer;
 import com.davy.reservation_system.user_service.entities.User;
 import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
+
+import static com.davy.reservation_system.user_service.config.RabbitMQConfig.USER_QUEUE;
 
 @Service
 public class UserProducer {
@@ -10,7 +13,9 @@ public class UserProducer {
     private AmqpTemplate amqpTemplate;
 
     public void sendUserCreated(User user) {
-        amqpTemplate.convertAndSend(" userQueue", user.getUsername());
+
+        amqpTemplate.convertAndSend(USER_QUEUE, user.getUsername());
+        System.out.println("Mensage sent to the queue: " + USER_QUEUE + " : " + user.getUsername());
     }
 
 }
