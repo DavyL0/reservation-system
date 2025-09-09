d9ac2011968225ac54c1ae4c79a543b1f290b157
Davy Lopes <d4vyl0pes@gmail.com>
2025-09-09 15:42:57 -0300

    update: add localhost config and add message methods

--- reservation-system.notification-service/src/main/java/com/davy/reservation_system/notification_service/service/UserConsumer.java
+++ reservation-system.notification-service/src/main/java/com/davy/reservation_system/notification_service/service/UserConsumer.java
@@ -1,4 +1,14 @@
 package com.davy.reservation_system.notification_service.service;
 
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
+
+import static com.davy.reservation_system.notification_service.config.RabbitMQConfig.USER_QUEUE;
+
+@Service
 public class UserConsumer {
+    @RabbitListener(queues = USER_QUEUE)
+    public void consumerMEnsage(String message){
+        System.out.println("📨 [Notification Service] Novo usuário criado: " + message);
+    }
 }
